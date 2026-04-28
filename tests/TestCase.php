c79573e1ce53f38029e44b60ea238b3a1b025215
Juan Litvin <litvinjuan@gmail.com>
2026-04-28 16:09:46 -0300

    add support for laravel 13

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -20,6 +20,7 @@ class TestCase extends Orchestra
     protected function getPackageProviders($app)
     {
         return [
+            \Laravel\Socialite\SocialiteServiceProvider::class,
             AHAuthServiceProvider::class,
         ];
     }
